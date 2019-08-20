b4fb4efa99ba697f45f8224decd20f70da1613a1
Juxley <juxley@protonmail.com>
2019-08-20 12:18:27 -0600

    Added createNewBlock method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,4 +1,20 @@
 function Blockchain() {
   this.chain = []
-  this.newTransaction = []
+  this.newTransactions = []
+}
+
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+  const newBlock = {
+    index: this.chain.length + 1, // what block you are on
+    timestamp: Date.now(),
+    transactions: this.newTransactions // New transcation that have been created after the previous block was mined
+    nonce,
+    hash,
+    previousBlockHash 
+  }
+
+  this.newTransactions = [] // clear out transactions
+  this.chain.push(newBlock)
+  
+  return newBlock
 }
