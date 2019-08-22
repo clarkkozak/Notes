a26e6db3a9c51bc4c108011b76bbf80e92fb1478
Juxley <juxley@protonmail.com>
2019-08-22 10:02:44 -0600

    Created hashing function
    Added:
      - sha256 modules and hashblock function

--- dev/test.js
+++ dev/test.js
@@ -2,9 +2,22 @@ const Blockchain = require('./blockchain')
 
 const coin = new Blockchain()
 
-coin.createNewBlock(1111, 'asfasdkjfasd;faj', 'asfdasdfasdfsa')
-coin.createNewTransaction('100', 'ALEX', 'MICHELE')
-console.log(coin)
-coin.createNewBlock(8119, 'asfdasdfasdfsa', 'yesyesyesyes') // A block needs to be mined for pending transactions to be added
-console.log(coin)
+const prevBlockHash = 'ASDJAHDASJDAHSD81273'
+const currBlockData = [ 
+  {
+    amount: 10,
+    sender: 'Lisa',
+    recipient: 'Rachel'
+  }
+]
+
+const nonce = 3212
+
+const hash = coin.hashBlock(prevBlockHash, currBlockData, nonce)
+console.log(hash)
+// coin.createNewBlock(1111, 'asfasdkjfasd;faj', 'asfdasdfasdfsa')
+// coin.createNewTransaction('100', 'ALEX', 'MICHELE')
+// console.log(coin)
+// coin.createNewBlock(8119, 'asfdasdfasdfsa', 'yesyesyesyes') // A block needs to be mined for pending transactions to be added
+// console.log(coin)
 
