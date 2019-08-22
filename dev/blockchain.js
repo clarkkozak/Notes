be8f057431d43cbaa187c70b9259d779878b91e7
Juxley <juxley@protonmail.com>
2019-08-22 09:48:51 -0600

    Added createNewTransactions and getLastBlock
    Changed:
      - changed newTransactions to pendingTransactions

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,22 +1,38 @@
 function Blockchain() {
   this.chain = []
-  this.newTransactions = []
+  this.pendingTransactions = []
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
   const newBlock = {
     index: this.chain.length + 1, // what block you are on
     timestamp: Date.now(),
-    transactions: this.newTransactions, // New transcation that have been created after the previous block was mined
+    transactions: this.pendingTransactions, // Transcation that have been created after the previous block was mined
     nonce,
     hash,
     previousBlockHash 
   }
 
-  this.newTransactions = [] // clear out transactions
+  this.pendingTransactions = [] // clear out transactions
   this.chain.push(newBlock)
   
   return newBlock
+}
+
+Blockchain.prototype.getLastBlock = function() {
+  return this.chain[this.chain.length = 1]
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient ) { // This can be any data, no only transctions
+  const newTransactions = {
+    amount,
+    sender,
+    recipient
+  }
+
+  this.pendingTransactions.push(newTransactions)
+
+  return this.getLastBlock()['index'] + 1
 }
 
 module.exports = Blockchain
