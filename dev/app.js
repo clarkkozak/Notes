fa72a511417799a2716a77f478a7b1e8c0a26425
Juxley <juxley@protonmail.com>
2019-08-23 11:54:26 -0600

    Added Blockchain constructor function to API

--- dev/app.js
+++ dev/app.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 7777
 
+const Blockchain = require('./blockchain')
+
+const ledger = new Blockchain()
+
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/blockchain', (req, res) => {
-  res.send('works')
+  res.send(ledger)
 })
 
 app.post('/transaction', (req, res) => {
