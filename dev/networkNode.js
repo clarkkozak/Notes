9bfa3a3f8ce648701641b14f2990a7845f468f6f
Juxley <juxley@protonmail.com>
2019-08-23 21:53:27 -0600

    Renamed app.js to networkNode.js
    Added:
      - npm scripts for mutiple testing nodes
      - node address for each node
      - network node array to constructor

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 7777
+const port = process.env.PORT || process.argv[2] 
 
 const uuid = require('uuid/v1')
 const nodeAddress = uuid().split('-').join('')
