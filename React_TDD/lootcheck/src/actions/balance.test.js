6ae9e7b20a3c1aff34840d2c31c780faf3323ce0
Clark Kozak <clarkkozak@protonmail.com>
2020-04-30 15:54:16 -0600

    Lesson 25. Adds withdraw and deposit to redux

--- React_TDD/lootcheck/src/actions/balance.test.js
+++ React_TDD/lootcheck/src/actions/balance.test.js
@@ -8,3 +8,19 @@ it('creates an action to set the balance', () => {
 
   expect(actions.setBalance(balance)).toEqual(expectedAction)
 })
+
+it('creates an action to despoit into the balance', () => {
+  const deposit = 10
+
+  const expectedAction = { type: constants.DEPOSIT, deposit } 
+
+  expect(actions.deposit(deposit)).toEqual(expectedAction)
+})
+
+it('creates an action to withdraw from the balance', () => {
+  const withdraw = 5
+
+  const expectAction = { type: constants.WITHDRAW, withdraw }
+
+  expect(actions.withdraw(withdraw)).toEqual(expectAction)
+})
--- React_TDD/lootcheck/src/reducers/balance.test.js
+++ React_TDD/lootcheck/src/reducers/balance.test.js
@@ -1,5 +1,6 @@
 import balanceReducer from './balance'
 import * as constants from '../actions/constants'
+import { withdraw } from '../actions/balance';
 
 describe('balanceReducer', () => {
 
@@ -8,6 +9,22 @@ describe('balanceReducer', () => {
     
     expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
       .toEqual(balance)
+  })
+
+  it('deposits into the balance', () =>{
+    const deposit = 10 
+    const initalState =  5
+
+    expect(balanceReducer(initalState, { type: constants.DEPOSIT, deposit }))
+      .toEqual(initalState + deposit)
+  })
+
+  it('withdraws from the balance', () => {
+    const withdarw = 1
+    const initalState = 5
+
+    expect(balanceReducer(initalState, { type: constants.WITHDRAW, withdarw }))
+      .toEqual(initalState - withdraw)
   })
 
 })
