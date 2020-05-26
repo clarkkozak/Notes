8f04c2d5b775a1b25621969638264f23aa369ca5
Clark Kozak <clarkkozak@protonmail.com>
2020-05-26 15:34:42 -0400

    Section 2, lesson 1 - 6

--- ultimate-redux/code/redux-starter/src/index.js
+++ ultimate-redux/code/redux-starter/src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import { compose, pipe } from "lodash/fp";
+let input = "    JavaScript   "
+let output = "<div>" + input.trim() + "</div>";
+
+const trim = (str) => str.trim()
+const wrapInDiv = (str) => `<div>${str}</div>`
+const toLowerCase = (str) => str.toLowerCase()
+const log = (str) => console.log(str)
+
+// functional compositation
+log(wrapInDiv(toLowerCase(trim(input))))
+
+// run all functions from right to left...still
+const transform = compose(log, wrapInDiv, toLowerCase, trim)
+transform(input)
+
+// run all functions from left to right
+const transformCorrectly = pipe(trim,toLowerCase, wrapInDiv, log)
+transformCorrectly(input)
