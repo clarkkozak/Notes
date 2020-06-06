f1a8628c9644058e1737b3219943b32f9f9a9104
Clark Kozak <clarkkozak@protonmail.com>
2020-06-06 10:10:17 -0400

    Session 3 lesson 8

--- ultimate-redux/code/redux-starter/src/reducer.js
+++ ultimate-redux/code/redux-starter/src/reducer.js
@@ -1,7 +1,47 @@
-// 
+// we assumed it was an []
 
-function reducer(state, action) {
-  if (action.type === 'bugAdded') {
-    
+let lastId = 0;
+
+// // state = [] will be our intial state
+// function reducer(state = [], action) {
+//   if (action.type === 'bugAdded') {
+//     return [
+//       ...state, // use immutablity library later
+//       {
+//         // In reducers, have the payload to have the
+//         // minimum amount of data
+//         // We can implement buisness logic in the reducer
+//         id: ++lastId,
+//         description: action.payload.description,
+//         resolved: false,
+//       },
+//     ];
+//   } else if (action.type === 'bugRemoved') {
+//     return state.filter((bug) => bug.id !== action.payload.id);
+//   }
+
+//   // If action doesn't exist, return state.
+//   return state
+// }
+
+// we can use switch and case too
+export default function reducer(state = [], action) {
+  switch (action.type) {
+    case 'bugAdded':
+      return [
+        ...state, // use immutablity library later
+        {
+          // In reducers, have the payload to have the
+          // minimum amount of data
+          // We can implement buisness logic in the reducer
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case 'bugRemoved':
+      return state.filter((bug) => bug.id !== action.payload.id);
+    default:
+      return state;
   }
 }
