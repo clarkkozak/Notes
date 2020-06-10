6bfc392ec82319b3bd5caa0d5aced44ea7394c68
Clark Kozak <clarkkozak@protonmail.com>
2020-06-10 14:11:28 -0400

    Checkpoint

--- Firebase/just-clock-it/src/components/AddTimeForm.jsx
+++ Firebase/just-clock-it/src/components/AddTimeForm.jsx
@@ -2,6 +2,146 @@ import React, { useState } from "react";
 
 import firebase from "../firebase";
 
+// const on = true
+// if (on) {
+// (async function () {
+//   const item = await firebase.firestore().collection('t8').get();
+
+//   console.log(item.docs.length);
+// })();
+// } else {
+// (async function () {
+//   const collection = 't8';
+
+//   function createEmptyInfoObject({ sign = '', planet = '', house = '' }) {
+//     return {
+//       title: sign + planet + house,
+//       sign,
+//       planet,
+//       house,
+//       ritual: '',
+//       meaning: '',
+//       element: sign2Element[sign] || '',
+//       imgRitual: '',
+//       imgMeaning: '',
+//     };
+//   }
+
+//   const signs = [
+//     'Aries',
+//     'Taurus',
+//     'Gemini',
+//     'Cancer',
+//     'Leo',
+//     'Virgo',
+//     'Libra',
+//     'Scorpio',
+//     'Sagittarius',
+//     'Capricorn',
+//     'Aquarius',
+//     'Pisces',
+//   ];
+
+//   const planets = [
+//     'Sun',
+//     'Moon',
+//     'Mercury',
+//     'Venus',
+//     'Mars',
+//     'Jupiter',
+//     'Saturn',
+//     'Uranus',
+//     'Neptune',
+//     'Pluto',
+//   ];
+
+//   const houses = [
+//     '1',
+//     '2',
+//     '3',
+//     '4',
+//     '5',
+//     '6',
+//     '7',
+//     '8',
+//     '9',
+//     '10',
+//     '11',
+//     '12',
+//   ];
+
+//   const sign2Element = {
+//     Aries: 'Fire',
+//     Taurus: 'Earth',
+//     Gemini: 'Air',
+//     Cancer: 'Water',
+//     Leo: 'Fire',
+//     Virgo: 'Earth',
+//     Libra: 'Air',
+//     Scorpio: 'Water',
+//     Sagittarius: 'Fire',
+//     Capricorn: 'Earth',
+//     Aquarius: 'Air',
+//     Pisces: 'Water',
+//   };
+
+//   const db = firebase.firestore().collection(collection)
+//   const res = []
+
+//   const item = await db.limit(1).get();
+//   if (item.empty) {
+//     const tracker = [];
+//     for (const sign of signs) {
+//       if (!tracker.includes(sign)) {
+//         // sign
+//         tracker.push(sign);
+//         const newDoc = createEmptyInfoObject({ sign })
+//         res.push(newDoc)
+//       }
+//       for (const planet of planets) {
+//         // planet
+//         if (!tracker.includes(planet)) {
+//           tracker.push(planet);
+//           const newDoc = createEmptyInfoObject({ planet })
+//           res.push(newDoc)
+//         }
+//         // sign-planent
+//         if (!tracker.includes(`${sign}-${planet}`)) {
+//           tracker.push(`${sign}-${planet}`);
+//             const newDoc = { sign, planet }
+//             res.push(newDoc)
+//         }
+//         for (const house of houses) {
+//           // sign-house
+//           if (!tracker.includes(`${sign}-${house}`)) {
+//             tracker.push(`${sign}-${house}`);
+//             const newDoc = { sign, planet }
+//             res.push(newDoc)
+
+//           }
+//           // sign-house
+//           if (!tracker.includes(`${planet}-${house}`)) {
+//             tracker.push(`${planet}-${house}`);
+//             const newDoc = { planet, house }
+//               res.push(newDoc)
+//           }
+//             const newDoc = { sign, planet, house }
+//             res.push(newDoc)
+//         }
+//       }
+//     }
+//   } else {
+//     console.log('collection exists');
+//   }
+
+//   if (res.length === 1846) {
+//     res.forEach(async item => {
+//       await db.add(item)
+//     })
+//   }
+// })();
+// }
+
 function AddTimeForm() {
   const [title, setTitle] = useState("");
   const [time, setTime] = useState("");
--- Firebase/just-clock-it/src/firebase.js
+++ Firebase/just-clock-it/src/firebase.js
@@ -3,14 +3,15 @@ import 'firebase/firestore';
 
 // Don't be a fool!
 var firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APPID,
+  apiKey: "AIzaSyCREW6FsNodF1SD3sTUZsoVDJWQ_KCecmI",
+  authDomain: "playpen-a6284.firebaseapp.com",
+  databaseURL: "https://playpen-a6284.firebaseio.com",
+  projectId: "playpen-a6284",
+  storageBucket: "playpen-a6284.appspot.com",
+  messagingSenderId: "1045121918796",
+  appId: "1:1045121918796:web:c053837bd6aa87351851e9"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
