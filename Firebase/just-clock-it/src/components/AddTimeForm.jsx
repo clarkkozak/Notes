cbfecbbdf84ef9b4b77fe0bff115d55f4b4e5dfb
Clark Kozak <clarkkozak@protonmail.com>
2020-06-11 16:55:01 -0400

    Checkpoint

--- Firebase/just-clock-it/src/components/AddTimeForm.jsx
+++ Firebase/just-clock-it/src/components/AddTimeForm.jsx
@@ -2,145 +2,146 @@ import React, { useState } from "react";
 
 import firebase from "../firebase";
 
-// const on = true
-// if (on) {
-// (async function () {
-//   const item = await firebase.firestore().collection('t8').get();
+const on = true
+if (on) {
+(async function () {
+  const item = await firebase.firestore().collection('info-objects').get();
 
-//   console.log(item.docs.length);
-// })();
-// } else {
-// (async function () {
-//   const collection = 't8';
+  console.log(item.docs.length);
+})();
+} else {
+(async function () {
+  const collection = 'info-objects';
 
-//   function createEmptyInfoObject({ sign = '', planet = '', house = '' }) {
-//     return {
-//       title: sign + planet + house,
-//       sign,
-//       planet,
-//       house,
-//       ritual: '',
-//       meaning: '',
-//       element: sign2Element[sign] || '',
-//       imgRitual: '',
-//       imgMeaning: '',
-//     };
-//   }
+  function createEmptyInfoObject({ sign = '', planet = '', house = '' }) {
+    return {
+      title: sign + planet + house,
+      sign,
+      planet,
+      house,
+      ritual: '',
+      meaning: '',
+      element: sign2Element[sign] || '',
+      imgRitual: '',
+      imgMeaning: '',
+    };
+  }
 
-//   const signs = [
-//     'Aries',
-//     'Taurus',
-//     'Gemini',
-//     'Cancer',
-//     'Leo',
-//     'Virgo',
-//     'Libra',
-//     'Scorpio',
-//     'Sagittarius',
-//     'Capricorn',
-//     'Aquarius',
-//     'Pisces',
-//   ];
+  const signs = [
+    'Aries',
+    'Taurus',
+    'Gemini',
+    'Cancer',
+    'Leo',
+    'Virgo',
+    'Libra',
+    'Scorpio',
+    'Sagittarius',
+    'Capricorn',
+    'Aquarius',
+    'Pisces',
+  ];
 
-//   const planets = [
-//     'Sun',
-//     'Moon',
-//     'Mercury',
-//     'Venus',
-//     'Mars',
-//     'Jupiter',
-//     'Saturn',
-//     'Uranus',
-//     'Neptune',
-//     'Pluto',
-//   ];
+  const planets = [
+    'Sun',
+    'Moon',
+    'Mercury',
+    'Venus',
+    'Mars',
+    'Jupiter',
+    'Saturn',
+    'Uranus',
+    'Neptune',
+    'Pluto',
+  ];
 
-//   const houses = [
-//     '1',
-//     '2',
-//     '3',
-//     '4',
-//     '5',
-//     '6',
-//     '7',
-//     '8',
-//     '9',
-//     '10',
-//     '11',
-//     '12',
-//   ];
+  const houses = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12',
+  ];
 
-//   const sign2Element = {
-//     Aries: 'Fire',
-//     Taurus: 'Earth',
-//     Gemini: 'Air',
-//     Cancer: 'Water',
-//     Leo: 'Fire',
-//     Virgo: 'Earth',
-//     Libra: 'Air',
-//     Scorpio: 'Water',
-//     Sagittarius: 'Fire',
-//     Capricorn: 'Earth',
-//     Aquarius: 'Air',
-//     Pisces: 'Water',
-//   };
+  const sign2Element = {
+    Aries: 'Fire',
+    Taurus: 'Earth',
+    Gemini: 'Air',
+    Cancer: 'Water',
+    Leo: 'Fire',
+    Virgo: 'Earth',
+    Libra: 'Air',
+    Scorpio: 'Water',
+    Sagittarius: 'Fire',
+    Capricorn: 'Earth',
+    Aquarius: 'Air',
+    Pisces: 'Water',
+  };
 
-//   const db = firebase.firestore().collection(collection)
-//   const res = []
+  const db = firebase.firestore().collection(collection)
+  const res = []
 
-//   const item = await db.limit(1).get();
-//   if (item.empty) {
-//     const tracker = [];
-//     for (const sign of signs) {
-//       if (!tracker.includes(sign)) {
-//         // sign
-//         tracker.push(sign);
-//         const newDoc = createEmptyInfoObject({ sign })
-//         res.push(newDoc)
-//       }
-//       for (const planet of planets) {
-//         // planet
-//         if (!tracker.includes(planet)) {
-//           tracker.push(planet);
-//           const newDoc = createEmptyInfoObject({ planet })
-//           res.push(newDoc)
-//         }
-//         // sign-planent
-//         if (!tracker.includes(`${sign}-${planet}`)) {
-//           tracker.push(`${sign}-${planet}`);
-//             const newDoc = { sign, planet }
-//             res.push(newDoc)
-//         }
-//         for (const house of houses) {
-//           // sign-house
-//           if (!tracker.includes(`${sign}-${house}`)) {
-//             tracker.push(`${sign}-${house}`);
-//             const newDoc = { sign, planet }
-//             res.push(newDoc)
+  const item = await db.limit(1).get();
+  if (item.empty) {
+    const tracker = [];
+    for (const sign of signs) {
+      if (!tracker.includes(sign)) {
+        // sign
+        tracker.push(sign);
+        const newDoc = createEmptyInfoObject({ sign })
+        res.push(newDoc)
+      }
+      for (const planet of planets) {
+        // planet
+        if (!tracker.includes(planet)) {
+          tracker.push(planet);
+          const newDoc = createEmptyInfoObject({ planet })
+          res.push(newDoc)
+        }
+        // sign-planent
+        if (!tracker.includes(`${sign}-${planet}`)) {
+          tracker.push(`${sign}-${planet}`);
+            const newDoc = createEmptyInfoObject({ sign, planet })
+            res.push(newDoc)
+        }
+        for (const house of houses) {
+          // sign-house
+          if (!tracker.includes(`${sign}-${house}`)) {
+            tracker.push(`${sign}-${house}`);
+            const newDoc = createEmptyInfoObject({ sign, house })
+            res.push(newDoc)
 
-//           }
-//           // sign-house
-//           if (!tracker.includes(`${planet}-${house}`)) {
-//             tracker.push(`${planet}-${house}`);
-//             const newDoc = { planet, house }
-//               res.push(newDoc)
-//           }
-//             const newDoc = { sign, planet, house }
-//             res.push(newDoc)
-//         }
-//       }
-//     }
-//   } else {
-//     console.log('collection exists');
-//   }
+          }
+          // sign-house
+          if (!tracker.includes(`${planet}-${house}`)) {
+            tracker.push(`${planet}-${house}`);
+            const newDoc = createEmptyInfoObject({ planet, house })
+              res.push(newDoc)
+          }
+            const newDoc = createEmptyInfoObject({ sign, planet, house })
+            res.push(newDoc)
+        }
+      }
+    }
+  } else {
+    console.log('collection exists');
+  }
 
-//   if (res.length === 1846) {
-//     res.forEach(async item => {
-//       await db.add(item)
-//     })
-//   }
-// })();
-// }
+  if (res.length === 1846) {
+    res.forEach(async item => {
+      await db.add(item)
+    })
+  }
+})();
+}
+
 
 function AddTimeForm() {
   const [title, setTitle] = useState("");
