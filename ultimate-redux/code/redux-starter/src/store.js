2bd7d70cb07fcd6d4c59d2a7718660f492f476fa
Clark Kozak <clarkkozak@protonmail.com>
2020-06-26 13:57:33 -0400

    Set up Tracing

--- ultimate-redux/code/redux-starter/src/store.js
+++ ultimate-redux/code/redux-starter/src/store.js
@@ -1,12 +1,13 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
+import { devToolsEnhancer } from 'redux-devtools-extension'
 
 // createStore is a higher order function,
 // it takes a function as an argument
 const store = createStore(
   reducer,
    // @ts-ignore
-   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   devToolsEnhancer({ trace: true })
   );
 
 
