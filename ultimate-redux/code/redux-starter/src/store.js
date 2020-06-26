3c3872c85a8cd2fa6e0d0408e49ad18a532d58cf
Clark Kozak <clarkkozak@protonmail.com>
2020-06-26 13:41:01 -0400

    Setup Redux Tool Kit

--- ultimate-redux/code/redux-starter/src/store.js
+++ ultimate-redux/code/redux-starter/src/store.js
@@ -3,7 +3,11 @@ import reducer from './reducer';
 
 // createStore is a higher order function,
 // it takes a function as an argument
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+   // @ts-ignore
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 
 // console.log(store)
