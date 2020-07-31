eefa3ff52169c7d2bcc1e8826353741408bae0fa
Clark Kozak <clarkkozak@protonmail.com>
2020-07-30 22:09:58 -0600

    ultimate_redux: S7L5
    Refactor store to use RTK

--- ultimate-redux/code/redux-starter/src/store/configureStore.js
+++ ultimate-redux/code/redux-starter/src/store/configureStore.js
@@ -1,13 +1,10 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import reducer from './bugs';
-import { devToolsEnhancer } from 'redux-devtools-extension'
 
 // createStore is a higher order function,
 // it takes a function as an argument
-export default function configureStore() {
-  const store = createStore(
-    reducer,
-    devToolsEnhancer({ trace: true })
-  );
-  return store
+export default function () {
+  return configureStore({
+    reducer
+  })
 }
