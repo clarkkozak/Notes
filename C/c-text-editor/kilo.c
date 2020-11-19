d735dc4d171460cbc996d45349fd956fea9b1b7f
Clark Kozak <clarkkozak@protonmail.com>
2020-11-18 18:41:11 -0700

    tutorial(c-text-editor): Step 8 Display Keypresses
    + vscode tasks to make kilo

--- C/c-text-editor/kilo.c
+++ C/c-text-editor/kilo.c
@@ -1,12 +1,21 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMod() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-  struct termios raw;
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMod);
 
-  tcgetattr(STDIN_FILENO, &raw);
-
-  raw.c_lflag &= ~(ECHO);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -14,6 +23,12 @@ void enableRawMode() {
 int main() {
   enableRawMode();
   char c;
-  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);    
+    } else {
+      printf("%d ('%c') \n", c, c);   
+    }
+  }
   return 0;
 }
