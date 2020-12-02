d94418ac44fc6233caa466ee8d2b204ccc1729c1
Clark Kozak <clarkkozak@protonmail.com>
2020-12-02 11:25:47 -0700

    tutorial(kilo.c): step 26

--- C/c-text-editor/kilo.c
+++ C/c-text-editor/kilo.c
@@ -1,3 +1,5 @@
+// includes
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,28 +7,45 @@
 #include <unistd.h>
 #include <errno.h>
 
-struct termios orig_termios;
+// defines
 
-void die(const char *s)
+#define CTRL_KEY(k) ((k)&0x1f)
+
+// data
+
+struct editorConfig
 {
+  struct termios orig_termios;
+};
+
+struct editorConfig E;
+
+// terminal
+
+void
+die(const char *s)
+{
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
 
 void disableRawMod()
 {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     die("tcsetattr");
 }
 
 void enableRawMode()
 {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
     die("tcgetattr");
 
   atexit(disableRawMod);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_cflag |= (CS8);
   raw.c_oflag &= ~(OPOST);
@@ -40,26 +59,62 @@ void enableRawMode()
     die("tcsetattr");
 }
 
+char editorReadKey()
+{
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+  {
+    if (nread == -1 && errno != EAGAIN)
+      die("read");
+  }
+  return c;
+}
+
+// output
+
+void editorDrawRows()
+{
+  int y;
+  for (y = 0; y < 24; y++)
+    write(STDOUT_FILENO, "~\r\n", 3);
+}
+
+void editorRefreshScreen()
+{
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+// input
+
+void editorProcessKeypress()
+{
+  char c = editorReadKey();
+
+  switch (c)
+  {
+  case CTRL_KEY('q'):
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    exit(0);
+    break;
+  }
+}
+
+// init
+
 int main()
 {
   enableRawMode();
   while (1)
   {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    read(STDIN_FILENO, &c, 1);
-    if (iscntrl(c))
-    {
-      // printf("%d\n", c);
-      printf("%d\r\n", c);
-    }
-    else
-    {
-      // printf("%d ('%c') \n", c, c);
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == 'q')
-      break;
+    editorRefreshScreen();
+    editorProcessKeypress();
   }
   return 0;
 }
