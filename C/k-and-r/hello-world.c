cfd0cdccccdd346f3695870afc3602f16e402c9a
Clark Kozak <clarkkozak@protonmail.com>
2020-12-04 10:28:08 -0700

    book(k&r) - hello world

---
+++ C/k-and-r/hello-world.c
@@ -0,0 +1,17 @@
+#include <stdio.h> // include information about standard library
+
+main() // define a function called main that received no argument values and returns no types!
+{
+//statements of main are enclosed in braces main calls library function printf
+//to print this sequence of characters
+///n represents the newline character
+printf("hello, world\n"); 
+// printf("\c"); // \c doesn't exist.
+// printf("\e"); // erasing a letter?
+// printf("test\btest\n"); // backspace
+
+// Identical to line 8
+printf("hello, ");
+printf("world");
+printf("\n");
+}
