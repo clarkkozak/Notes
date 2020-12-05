f326257ed55450ece3215315b293c58918652adc
Clark Kozak <clarkkozak@protonmail.com>
2020-12-05 16:09:42 -0700

    book(k&r) - exercise 1-5
    Exercise 1-5.
    Modify the temperature conversion program
    to print the table in reverse order,
    that is, from 300 degrees to 0.

--- C/k-and-r/fahrenheit-celsius.c
+++ C/k-and-r/fahrenheit-celsius.c
@@ -4,61 +4,15 @@
 /* print Fahrenheit-Celsius table
    for fahr - 0, 20, ..., 300 */
 
-main()
+int main()
 {
-  // declarations, with type integer (rather than a float)
-  /*
-  The range of both int and  float depends on the machine you are using; 
-  16-bits int s, which lie between -32768 and +32767, are common, as are 32-bit ints.
-  A float number is typically a 32-bit quantity, 
-  with   at least six significant digits and magnitude generally between about 10^-38 and 10^38 .
-  */
-  float fahr, celsius; 
-  float lower, upper, step;
+  // Exercise 1-5. 
+  // Modify the temperature conversion program to print the table in reverse order,
+  // that is, from 300 degrees to 0.
+  int fahr; 
 
-  /*
-    int and float , including:
-    char character - a single byte
-    short short integer
-    long long integer
-    double double-precision floating point
-  */
-  // also arrays, structures, and unions, pointers and functions that can return them
-
-
-  // assignment
-  lower = 0; // lower limit of the temperature range on table
-  upper = 300;  // upper limit 
-  step = 20; // step size on the table
-  // Individual statements are terminated by semicolons.
-
-  fahr = lower;
-
-  // a loop
-  // if with only one statement, doesn't need {} if indentation is used
-  // placement of brackets aren't relavant, aim to make your code easy for people to read
-  // Pick a style that suits you, then use it consistently.
-  while(fahr <= upper) {
-    // celsius = 5 * (fahr - 32) / 9; // integer division truncates: any fractional part is discarded
-    // integers 
-    // printf("%d\t%d\n", fahr, celsius); 
-    // printf("%3d %6d\n", fahr, celsius); // to print the first number of each line in a field three digits wide, and the second in a field six digits wide
-    // fahr = fahr + step;
-    
-    // By the way, printf is not part of the C language; there is no input or output defined in C
-    // itself. printf is just a useful function from the standard library of functions that are normally
-    // accessible to C programs
-
-    // floats
-
-    // writing floating-point constants with explicit decimal points even when they have integral values
-    // emphasizes their floating-point nature for human readers.
-    celsius = (5.0/9.0) * (fahr-32.0);
-    printf("%3.0f %6.1f\n", fahr, celsius);
-    fahr = fahr + step;
- 
-
-  }
+  for (fahr = 300; fahr > 0; fahr = fahr - 20)
+    printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
   return 0;
 }
 
