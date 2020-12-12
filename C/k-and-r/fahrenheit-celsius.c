46a42891bdf61dbb85f6c2648448b5dfe629ccb1
Clark Kozak <clarkkozak@protonmail.com>
2020-12-12 09:24:22 -0700

    c(k&r) - symbolic constants

--- C/k-and-r/fahrenheit-celsius.c
+++ C/k-and-r/fahrenheit-celsius.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+// It's bad practice to bury "magic numbers" like 300 and 20 in a program;
+// A #define line defines a symbolic name or symbolic constant to be a particular string of characters:
+#define LOWER   0   /* lower limit of table */ 
+#define UPPER   300 /* upper limit */
+#define STEP    20  /* step size */
+
+// The quantities LOWER , UPPER and STEP are symbolic constants, not variables, so they do not appear in declarations. 
+//Symbolic constant names are conventionally written in upper case so they can ber readily distinguished from lower case variable names.
+
 // This is a comment
 /* print Fahrenheit-Celsius table
    for fahr - 0, 20, ..., 300 */
@@ -11,7 +20,7 @@ int main()
   // that is, from 300 degrees to 0.
   int fahr; 
 
-  for (fahr = 300; fahr > 0; fahr = fahr - 20)
+  for (fahr = UPPER; fahr > LOWER; fahr = fahr - STEP)
     printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
   return 0;
 }
