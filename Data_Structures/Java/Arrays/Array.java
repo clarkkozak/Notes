32d5e75baa2a8ca491a6e99b2ded0ef976c8f5ca
Clark Kozak <clarkkozak@protonmail.com>
2020-12-17 20:38:43 -0700

    data_structures(java) - fix removeAt
    I implmented a "pop" like solution instead of left shfiting.
    
    It took me a surprising amount of time to figure it out too!
    
    It was an interesting solution

--- Data_Structures/Java/Arrays/Array.java
+++ Data_Structures/Java/Arrays/Array.java
@@ -1,33 +1,49 @@
 package Arrays;
 
 public class Array {
-  private int size;
-  private int index;
+  private int count;
   private int[] array;
   
-  public Array(int size) {
-    this.size = size;
-    this.array = new int[size];
+  public Array(int length) {
+    this.array = new int[length];
   }
 
   public void insert(int number) {
-    if (size > index)
+    if (array.length == count)
       resize();
 
-    array[index++] = number;
+    array[count++] = number;
   }
 
   public void print() {
-    for (int i = 0; i < index; i++)
+    for (int i = 0; i < count; i++)
       System.out.println(array[i]);
   }
 
-  public int size() {
-    return index;
-  }
+  // [0, 1, 2]
+  public void removeAt(int index) {
+    int[] newArray = new int[array.length];
+    boolean targetFound = false;
+    if (index >= count || index < 0)
+      throw new IllegalArgumentException();
+    
+    for (int i = 0; i < newArray.length - 1 ; i++) {
+      if (i == index) {
+        targetFound = true;
+      } 
 
-  public void removeAt(int number) {
-    index--;
+      if (targetFound) {
+        newArray[i] = array[i + 1];
+      } else {
+        newArray[i] = array[i];
+      }
+
+
+    }
+
+    array = newArray;
+
+    count--;
   }
 
   public int indexOf(int number) {
@@ -39,12 +55,12 @@ public class Array {
   }
 
   private void resize() {
-    int[] newArray = new int[size * 2];
+    int[] newArray = new int[array.length * 2];
 
     for (int i = 0; i < array.length; i++)
       newArray[i] = array[i];
     
-    this.array = newArray;
+    array = newArray;
   }
 
 }
