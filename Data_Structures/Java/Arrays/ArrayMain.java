9c1429f9a0e73d5602d1a2c66b89b5ed99a348f1
Clark Kozak <clarkkozak@protonmail.com>
2020-12-17 21:16:29 -0700

    data_structures(java) - max item in an array
    kill all {} in Java >:D

--- Data_Structures/Java/Arrays/ArrayMain.java
+++ Data_Structures/Java/Arrays/ArrayMain.java
@@ -7,16 +7,10 @@ package Arrays;
 public class ArrayMain {
   
   public static void main(String[] args) {
-    // Exercise
     Array numbers = new Array(3);
-    numbers.insert(10);
+    numbers.insert(50);
     numbers.insert(20);
     numbers.insert(30);
-    numbers.insert(40);
-    numbers.removeAt(2);
-    // System.out.println(numbers.indexOf(10));// 0 
-    // System.out.println(numbers.indexOf(100)); // -1
-    numbers.print(); // 10, 20, 30
-
+    System.out.println(numbers.max());
   }
 }
