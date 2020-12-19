5386dfba7b934070784003f25e3921457a20bd29
Clark Kozak <clarkkozak@protonmail.com>
2020-12-19 09:34:14 -0700

    playgin(c-make) - playing with CMake

---
+++ C/c-make/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/clark/Projects/Notes/C/c-make
+# Build directory: /home/clark/Projects/Notes/C/c-make/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
