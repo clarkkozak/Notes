1cf1d61fe00f0bf6a8e58263bc5dcc475e17c97e
Clark Kozak <clarkkozak@protonmail.com>
2020-12-19 09:34:14 -0700

    playgin(c-make) - playing with CMake

---
+++ C/c-make/build/CMakeFiles/c-make-test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c-make-test.dir/main.cpp.o"
+  "c-make-test"
+  "c-make-test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c-make-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
