ef2d0929f8bec79f07521180617fdcbfa94b5756
Clark Kozak <clarkkozak@protonmail.com>
2020-12-23 12:54:09 -0700

    data_strcutres(java) - Array.intersects

--- Data_Structures/Java/Arrays/Array.java
+++ Data_Structures/Java/Arrays/Array.java
@@ -20,6 +20,10 @@ public class Array {
       System.out.println(array[i]);
   }
 
+  public int size() {
+    return count;
+  }
+
   // [0, 1, 2]
   public void removeAt(int index) {
     if (index >= count || index < 0)
@@ -31,9 +35,9 @@ public class Array {
     count--;
   }
 
-  public int indexOf(int number) {
+  public int indexOf(int value) {
     for (int i = 0; i < array.length; i++)
-      if (number == array[i])
+      if (value == array[i])
         return i;
     
     return -1;
@@ -56,5 +60,38 @@ public class Array {
         max = array[i];
 
     return max;
-  } 
+  }
+
+  public int getItem(int index) {
+    if (index > array.length - 1)
+      throw new IllegalArgumentException();
+
+    return array[index];
+  }
+
+  public boolean contains(int value) {
+    for (int i = 0; i < array.length; i++)
+    if (value == array[i])
+      return true;
+  
+    return false;
+  }
+
+  public Array intersects(Array compareArray) {
+    var smallerArraySize = compareArray.size() < this.count ? compareArray.size() : this.count;
+
+    Array intersectArray = new Array(smallerArraySize);
+
+    for (int i = 0; i < array.length; i++) {
+      if (intersectArray.size() == smallerArraySize)
+        break;
+      for (int j = 0; j < compareArray.size(); j++) {
+        if (array[i] == compareArray.getItem(j) && !intersectArray.contains(compareArray.getItem(j))) {
+          intersectArray.insert(array[i]);
+        }
+      }
+    }
+
+    return intersectArray;
+  }
 }
--- Data_Structures/Java/Arrays/ArrayMain.java
+++ Data_Structures/Java/Arrays/ArrayMain.java
@@ -8,9 +8,23 @@ public class ArrayMain {
   
   public static void main(String[] args) {
     Array numbers = new Array(3);
-    numbers.insert(50);
-    numbers.insert(20);
-    numbers.insert(30);
-    System.out.println(numbers.max());
+    numbers.insert(2); // 2 
+    numbers.insert(-166); 
+    numbers.insert(5); // 5
+    numbers.insert(30); // 30
+    numbers.insert(30);  
+
+    Array numbers2 = new Array(3);
+    numbers2.insert(30); // 30
+    numbers2.insert(59);
+    numbers2.insert(5); // 5
+    numbers2.insert(3);
+    numbers2.insert(2); // 2 
+    numbers2.insert(-19);
+    
+    
+    Array intersects = numbers2.intersects(numbers);
+    intersects.print();
+
   }
 }
