8f9c0a569e05e3b9dc0b619dabd73738b7bb1e7c
Clark Kozak <clarkkozak@protonmail.com>
2020-12-25 09:06:16 -0700

    data_structures(java) - my LinkedList part 2
    I think I'm done with this...
    I may want to refactor prior to viewing the answers

--- Data_Structures/Java/LinkedLists/LinkedListMain.java
+++ Data_Structures/Java/LinkedLists/LinkedListMain.java
@@ -3,7 +3,8 @@ package LinkedLists;
 public class LinkedListMain {
   public static void main(String[] args) {
     LinkedList list = new LinkedList();
-    list.addFirst(19);
+
+    list.deleteLast();
     list.print();
   }
 }
