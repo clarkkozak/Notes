cf00c83fafa2931873e97a796d9da2445af64be7
Clark Kozak <clarkkozak@protonmail.com>
2020-12-25 09:06:16 -0700

    data_structures(java) - my LinkedList part 2
    I think I'm done with this...
    I may want to refactor prior to viewing the answers

--- Data_Structures/Java/LinkedLists/LinkedList.java
+++ Data_Structures/Java/LinkedLists/LinkedList.java
@@ -5,24 +5,110 @@ public class LinkedList {
   private Node last;
 
   public LinkedList() {
-    this.first = new Node();
-    this.last = new Node();
+    this.first = null;
+    this.last = null;
   }
 
   public void print() {
-    System.out.println(first);
-    System.out.println(first.next);
+    var current = first;
+    while(current != null) {
+      System.out.println(current.value);
+      current = current.next;
+    }
   }
 
   // addFirst
   public void addFirst(int value) {
-    first.value = value;
+    if (isEmpty()) {
+      first = new Node(value);
+      last = first;
+    }
+    else {
+      var temp = first;
+      first = new Node(value);
+      first.next = temp; 
+    }
+  }
+
+  public boolean isEmpty() {
+    return first == null && last == null;
   }
 
   // addLast
+  public void addLast(int value) {
+    if (isEmpty()) {
+      first = new Node(value);
+      last = first;
+      return;
+    }
+
+    var current = first;
+    
+    while(current != null) {
+      if (current == last) {
+        current.next = new Node(value);
+        last = current.next;
+        break;
+      }
+      current = current.next;
+    }
+    
+  }
+
   // deleteFirst
+  public void deleteFirst() {
+    if (isEmpty())
+      throw new IllegalArgumentException();
+
+    first = first.next;
+  }
+
+
   // deleteLast
+  public void deleteLast() {
+    if (isEmpty())
+      throw new IllegalArgumentException();
+     
+    if (first.next == null) {
+      first = null;
+      return;
+    }  
+
+    var current = first;
+    Node previous = null;
+    while (current != null) {
+      if (current == last) {
+        previous.next = null;
+        last = previous;
+      }
+      previous = current;
+      current = current.next;
+    }
+
+  }
+  
   // contains
+  public boolean contains(int value) {
+    return indexOf(value) != -1;
+  }
+
   // indexOf
+  public int indexOf(int value) {
+    if (isEmpty())
+     return -1;
+      
+    var current = first;
+    int count = 0;
+
+    while (current != null) {
+      if (current.value == value)
+        return count;
+
+      current = current.next;
+      ++count;
+    }
+
+    return -1;
+  }
 
 }
--- Data_Structures/Java/LinkedLists/Node.java
+++ Data_Structures/Java/LinkedLists/Node.java
@@ -4,8 +4,13 @@ public class Node {
   public int value;
   public Node next;
 
+  public Node(int value) {
+    this.value = value;
+  }
+  
   @Override
   public String toString() {
     return String.valueOf(value);
   }
+
 }
