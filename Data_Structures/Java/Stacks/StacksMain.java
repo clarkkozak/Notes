7a575c25e87e44df7aad18e9842b1f0f38ae3ec8
Clark Kozak <clarkkozak@protonmail.com>
2020-12-30 20:58:53 -0700

    ds(java) - Answer: reverse a String with a Stack
    It's awesome how much one can learn from slowing down.

--- Data_Structures/Java/Stacks/StacksMain.java
+++ Data_Structures/Java/Stacks/StacksMain.java
@@ -12,18 +12,29 @@ public class StacksMain {
   }
 
   public static String reverse(String str) {
-    String revString = "";
+    // Null pointer exception!!!
+    if (str == null) {
+      throw new IllegalArgumentException();
+    }
+    
     Stack<Character> charStack = new Stack<>();
-
-    for (int i = 0; i < str.length(); i++) {
-      charStack.push(str.charAt(i));
-    }
-
+    
+    // for (int i = 0; i < str.length(); i++) {
+      for(char ch : str.toCharArray()) { // cleaner to work with a char than a index
+        charStack.push(ch);
+      }
+      
+    // Initialize before using, instead of having it at the top
+    // Use a StringBuffer for I/O, since Strings are immutable in Java
+    // String revString = "";
+    StringBuffer revString = new StringBuffer();
     while(!charStack.isEmpty()) {
-      revString += charStack.pop();
+      // revString += charStack.pop();
+      revString.append(charStack.pop());
     }
 
-    return revString;
+    return revString.toString();
+    // return revString.toString();
   }
 
 }
