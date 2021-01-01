81894f65c167e7f360449b5f517e81063c774a6d
Clark Kozak <clarkkozak@protonmail.com>
2021-01-01 10:13:39 -0700

    ds(java) - isBalancedAns part 2

--- Data_Structures/Java/Stacks/StacksMain.java
+++ Data_Structures/Java/Stacks/StacksMain.java
@@ -9,7 +9,7 @@ public class StacksMain {
     // System.out.println(name);
     // System.out.println(reverseName);
 
-    String expression = ")(()";
+    String expression = "([]>";
     System.out.println(isBalancedAns(expression));
 
   }
@@ -19,19 +19,19 @@ public class StacksMain {
     if (str == null) {
       throw new IllegalArgumentException();
     }
-    
+
     Stack<Character> charStack = new Stack<>();
-    
+
     // for (int i = 0; i < str.length(); i++) {
-      for(char ch : str.toCharArray()) { // cleaner to work with a char than a index
-        charStack.push(ch);
-      }
-      
+    for (char ch : str.toCharArray()) { // cleaner to work with a char than a index
+      charStack.push(ch);
+    }
+
     // Initialize before using, instead of having it at the top
     // Use a StringBuffer for I/O, since Strings are immutable in Java
     // String revString = "";
     StringBuffer revString = new StringBuffer();
-    while(!charStack.isEmpty()) {
+    while (!charStack.isEmpty()) {
       // revString += charStack.pop();
       revString.append(charStack.pop());
     }
@@ -42,10 +42,12 @@ public class StacksMain {
 
   public static boolean isBalanced(String str) {
 
-    if (str == null) throw new IllegalArgumentException();
+    if (str == null)
+      throw new IllegalArgumentException();
 
-    if (str == "" ) return false;
-    
+    if (str == "")
+      return false;
+
     Stack<Character> stack = new Stack<>();
     String openingBrackets = "[{<(";
     String closingBrackets = "]}>)";
@@ -59,7 +61,7 @@ public class StacksMain {
         // then unload the stack to check for next bracket
         while (!stack.isEmpty()) {
           char possibleBracket = stack.pop();
-          // if we have the right bracket balanced 
+          // if we have the right bracket balanced
           if (openingBrackets.indexOf(possibleBracket) == closingBrackets.indexOf(ch)) {
             // then get out of the loop
             break;
@@ -70,32 +72,45 @@ public class StacksMain {
             // then it isn't balanced
             return false;
           }
-        }    
+        }
       }
     }
 
     // If there's still something in the stack
-    while(!stack.isEmpty()) {
+    while (!stack.isEmpty()) {
       // check if it has a opening bracket without a closing bracket
       if (openingBrackets.indexOf(stack.pop()) != -1) {
         return false;
       }
     }
-    return true;    
+    return true;
   }
 
   public static boolean isBalancedAns(String str) {
     Stack<Character> stack = new Stack<>();
 
-  
     for (char ch : str.toCharArray()) {
       // why push anything but the chars we are looking for?
-      if (ch == '(') {
+      if (ch == '(' || ch == '<' || ch == '[' || ch == '{') {
         stack.push(ch);
       }
-      if (ch == ')') {
-        if (stack.isEmpty()) return false;
-        stack.pop();
+
+      if (ch == ')' || ch == '>' || ch == ']' || ch == '}') {
+        if (stack.isEmpty()) {
+          return false;
+        }
+
+        char top = stack.pop();
+        
+        if (
+          (ch == ')' && top != '(') ||
+          (ch == '>' && top != '<') || 
+          (ch == ']' && top != '[') ||
+          (ch == '}' && top != '{')
+        ) {
+          return false;
+        }
+
       }
     }
 
