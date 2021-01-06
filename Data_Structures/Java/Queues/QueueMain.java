8dc2fe99681b7a6428b191a2f44ea2bea3d7bb92
Clark Kozak <clarkkozak@protonmail.com>
2021-01-06 15:55:46 -0500

    ds(java) - Reverse a queue

--- Data_Structures/Java/Queues/QueueMain.java
+++ Data_Structures/Java/Queues/QueueMain.java
@@ -1,6 +1,9 @@
 package Queues;
 
 import java.util.Queue;
+
+import java.util.Stack;
+
 import java.util.ArrayDeque;
 
 public class QueueMain {
@@ -11,8 +14,21 @@ public class QueueMain {
     queue.add(20);
     queue.add(30);
     System.out.println(queue);
-    var front = queue.remove();
-    System.out.println(front);
+    reverse(queue);
     System.out.println(queue);
+
+
+  }
+
+  public static void reverse(Queue<Integer> queue) {
+    Stack<Integer> stack = new Stack<>();
+
+    while(!queue.isEmpty()) {
+      stack.push(queue.remove());
+    }
+
+    while (!stack.isEmpty()) {
+      queue.add(stack.pop());
+    }
   }
 }
