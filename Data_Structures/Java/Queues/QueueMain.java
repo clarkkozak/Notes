cc1327af4debe9c6809913da13ff8a89c151fdc4
Clark Kozak <clarkkozak@protonmail.com>
2021-01-07 14:42:21 -0500

    ds(java) - my ArrayQueue

--- Data_Structures/Java/Queues/QueueMain.java
+++ Data_Structures/Java/Queues/QueueMain.java
@@ -1,18 +1,18 @@
 package Queues;
 
-import java.util.Queue;
+// import java.util.Queue;
 
 import java.util.Stack;
 
-import java.util.ArrayDeque;
+// import java.util.ArrayDeque;
 
 public class QueueMain {
   public static void main(String[] args) {
-    Queue<Integer> queue = new ArrayDeque<>();
+    ArrayQueue queue = new ArrayQueue(5);
 
-    queue.add(10);
-    queue.add(20);
-    queue.add(30);
+    queue.enqueue(10);
+    queue.enqueue(20);
+    queue.enqueue(30);
     System.out.println(queue);
     reverse(queue);
     System.out.println(queue);
@@ -20,15 +20,15 @@ public class QueueMain {
 
   }
 
-  public static void reverse(Queue<Integer> queue) {
+  public static void reverse(ArrayQueue queue) {
     Stack<Integer> stack = new Stack<>();
 
     while(!queue.isEmpty()) {
-      stack.push(queue.remove());
+      stack.push(queue.dequeue());
     }
 
     while (!stack.isEmpty()) {
-      queue.add(stack.pop());
+      queue.enqueue(stack.pop());
     }
   }
 }
