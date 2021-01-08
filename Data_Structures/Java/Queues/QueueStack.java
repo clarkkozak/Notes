05af0813916b4bd579ea8a5396d92d44e0659d33
Clark Kozak <clarkkozak@protonmail.com>
2021-01-07 22:05:36 -0500

    ds(java) - QueueStack Ans
    It's like mine! Yet with the peek method as well
    
    Not bad!

--- Data_Structures/Java/Queues/QueueStack.java
+++ Data_Structures/Java/Queues/QueueStack.java
@@ -6,25 +6,41 @@ public class QueueStack {
   private Stack<Integer> stackBack = new Stack<>();
   private Stack<Integer> stackFront = new Stack<>();
 
-  // sB 30 20 10
-  // sF 10 20 30
-  //  q 10 20 30
+  // O(1)
   public void enqueue(int item) {
       stackBack.push(item);
   }
 
+  // O(n)
   public int dequeue() {
     if (isEmpty()) {
       throw new IllegalArgumentException();
     }
 
+    moveStackOneToStackTwo();
+
+    return stackFront.pop();
+  }
+
+  private void moveStackOneToStackTwo() {
     while (!stackBack.isEmpty()) {
       stackFront.push(stackBack.pop());
     }
-    return stackFront.pop();
   }
 
   public boolean isEmpty() {
     return stackBack.isEmpty() && stackFront.isEmpty();
   }
+
+  // O(n) 
+  // I forgot the peek method
+  public int peek() {
+    if (isEmpty()) {
+      throw new IllegalArgumentException();
+    }
+
+    moveStackOneToStackTwo();
+    
+    return stackFront.peek();
+  }  
 }
