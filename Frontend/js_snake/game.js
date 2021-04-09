16a156e4617a9244ccdc4a7d598cefd2d4ccb0d3
Clark Kozak <clarkkozak@protonmail.com>
2021-04-09 15:06:41 -0600

    js_snake: add failure state

--- Frontend/js_snake/game.js
+++ Frontend/js_snake/game.js
@@ -1,10 +1,16 @@
-import { SNAKE_SPEED, render as renderSnake, update as updateSnake  } from "./snake.js";
+import { SNAKE_SPEED, render as renderSnake, update as updateSnake, snakeIntersection, getSnakeHead  } from "./snake.js";
 import { render as renderFood, update as updateFood} from "./food.js";
+import { outsideGrid } from "./grid.js";
 let lastRenderTime = 0
 const gameBoard = document.getElementById('game-board')
+let gameOver = false
 
 
 function main(currentTime) {
+  if (gameOver) {
+    return alert('you lose')
+  }
+  
   window.requestAnimationFrame(main)
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
@@ -22,10 +28,16 @@ window.requestAnimationFrame(main)
 function update() {
   updateSnake()
   updateFood()
+  checkDeath()
 }
 
 function render() {
   gameBoard.innerHTML = ''
   renderSnake(gameBoard)
   renderFood(gameBoard)
+}
+
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
