fdb22eb9347bc3e773ccf98b07a193a638da8fd3
Clark Kozak <clarkkozak@protonmail.com>
2021-04-09 14:40:27 -0600

    js_snake: expand snake when it eats food
    I'm unsure on hte naming of "onSnake". I would think "onFood"

--- Frontend/js_snake/food.js
+++ Frontend/js_snake/food.js
@@ -1,6 +1,13 @@
+import { onSnake, expandSnake } from "./snake.js"
 let food = { x: 10, y: 1} // Zero is technically outside the grid
+const EXPANSION_RATE = 1
 
 export function update() {
+  if (onSnake(food)) {
+    console.log('tes')
+    expandSnake(EXPANSION_RATE) 
+    food = { x: 20, y: 10} // this will be random
+  }
 
 }
 
