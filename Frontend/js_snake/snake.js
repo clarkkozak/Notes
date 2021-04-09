ff5597ba3d7be6ea4308422ac5b5170f0f1e21ce
Clark Kozak <clarkkozak@protonmail.com>
2021-04-09 13:57:49 -0600

    js_snake: making the snake move
    note the as y increases the snake moves down
    
    we essential "shift" the array and update the head to move the snake
    
    we reset the gameBoard and redraw. I wonder how effect that is

--- Frontend/js_snake/snake.js
+++ Frontend/js_snake/snake.js
@@ -1,17 +1,24 @@
 export const SNAKE_SPEED = 2
 const snakeBody = [ 
-  {x: 11, y: 11} // the center of the board
+  {x: 10, y: 11}, // the center of the board
+  {x: 11, y: 11}, // the center of the board
+  {x: 12, y: 11} // the center of the board
 ]
 
 export function update() {
- console.log('update') 
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] }
+  }
+  
+  snakeBody[0].x += 0
+  snakeBody[0].y += 1
 }
 
 export function render(gameBoard) {
   for (const segament of snakeBody) {
     const snakeElement = document.createElement('div')
-    snakeElement.style.gridRowStart = segament.x
-    snakeElement.style.gridColumnStart = segament.y
+    snakeElement.style.gridRowStart = segament.y
+    snakeElement.style.gridColumnStart = segament.x
     snakeElement.classList.add('snake')
     gameBoard.appendChild(snakeElement)
   }
