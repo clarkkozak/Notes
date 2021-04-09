75596a13f963d79e23281a604a8bcfe748aef794
Clark Kozak <clarkkozak@protonmail.com>
2021-04-09 14:25:29 -0600

    js_snake: add keydown event listers.
    can't go backwards in snake. Add conditionals!

--- Frontend/js_snake/snake.js
+++ Frontend/js_snake/snake.js
@@ -1,17 +1,19 @@
-export const SNAKE_SPEED = 2
+import { getInputDirection } from "./input.js"
+
+export const SNAKE_SPEED = 8
+
 const snakeBody = [ 
-  {x: 10, y: 11}, // the center of the board
   {x: 11, y: 11}, // the center of the board
-  {x: 12, y: 11} // the center of the board
 ]
 
 export function update() {
+  const inputDireciton = getInputDirection()
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] }
   }
   
-  snakeBody[0].x += 0
-  snakeBody[0].y += 1
+  snakeBody[0].x += inputDireciton.x
+  snakeBody[0].y += inputDireciton.y
 }
 
 export function render(gameBoard) {
@@ -22,5 +24,4 @@ export function render(gameBoard) {
     snakeElement.classList.add('snake')
     gameBoard.appendChild(snakeElement)
   }
-
 }
