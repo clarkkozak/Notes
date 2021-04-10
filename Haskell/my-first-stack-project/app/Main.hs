cc81fc4a316d72e8c3e80d7d088f49c70064f1cb
Clark Kozak <clarkkozak@protonmail.com>
2021-04-10 12:47:52 -0600

    haskell: now that it compiles

---
+++ Haskell/my-first-stack-project/app/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Lib
+
+import Test
+
+main :: IO ()
+main = do
+  someFunc
+  another
---
+++ Haskell/my-first-stack-project/src/Lib.hs
@@ -0,0 +1,8 @@
+module Lib
+    ( someFunc
+    ) where
+
+import Data.List (unwords) 
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
