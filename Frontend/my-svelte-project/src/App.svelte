7090b3ee5f2b09018c383799159aaff3a69676b3
Clark Kozak <clarkkozak@protonmail.com>
2021-04-11 11:31:24 -0600

    svelete: reactivity

--- Frontend/my-svelte-project/src/App.svelte
+++ Frontend/my-svelte-project/src/App.svelte
@@ -1,10 +1,45 @@
 <script>
 	export let name;
+
+	let count = 0;
+	let numbers = [1, 2, 3, 4]
+
+	$: if (count >= 10) { // We're not limited to declaring reactive values — we can also run arbitrary statements reactively.
+		alert(`count is dangerously high`);
+		count = 9;
+	} 
+
+	$: doubled = count * 2;
+	
+	$: sum = numbers.reduce((t,n) => t + n, 0)
+
+	function handleClick() {
+		count += 1;
+	}
+	
+	function addNumber() {
+		numbers = [...numbers, numbers.length + 1]
+	}
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<button on:click={handleClick}>
+		Clicked {count}
+		{count === 1 ? "time" : "times"}
+	</button>
+	<p>{count} double is {doubled}</p>
+
+	<p>{numbers.join(' + ')} = {sum}</p>
+
+	<button on:click={addNumber}>
+		Add a number
+	</button>
 </main>
 
 <style>
