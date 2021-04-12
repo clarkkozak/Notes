72087aff10677d53acdfb19de02a2174e1ae4776
Clark Kozak <clarkkozak@protonmail.com>
2021-04-12 13:04:52 -0600

    svelte: event handling + little bit of binding

---
+++ Frontend/my-svelte-project/src/Inner.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { createEventDispatcher} from 'svelte'
+	
+	const dispatch = createEventDispatcher()
+	
+	function sayHello() {
+		dispatch('myevent', {
+			text: 'Hello from Inner.svelte',
+		})
+	}
+</script>
+
+<button on:click={sayHello}>
+	Click to say hello
+</button>
