d9ecbb1b05de452d8e63492346b335af954f941e
Clark Kozak <clarkkozak@protonmail.com>
2021-04-24 22:25:49 -0600

    haskell: p2 refactor
    I watched this video on infinte lists which helped my solution:
    
    https://www.youtube.com/watch?v=NvLOz-W4PsA

--- Haskell/project-euler/p2.hs
+++ Haskell/project-euler/p2.hs
@@ -1,20 +1,14 @@
 -- n = 1  2  3  4  5  6    7   8   9   10
 --    [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
-calcFibs :: Int -> [Int]
-calcFibs 0  = [1]
-calcFibs 1  = [1]
-calcFibs n = xs ++ [sum (takeLast2 xs)] 
-  where xs = calcFibs (n - 1)
-
-takeLast2 :: [Int] -> [Int]
-takeLast2 = reverse . take 2 . reverse 
+fibs :: [Int]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 isLessThan4Mil :: Int -> Bool
 isLessThan4Mil x = x < 4000000
 
 fibsLessThan4MilAndEven :: [Int]
-fibsLessThan4MilAndEven = filter even $ filter isLessThan4Mil $ calcFibs 50
+fibsLessThan4MilAndEven = filter even $ takeWhile isLessThan4Mil fibs
 
 solve :: Int
-solve = sum $ filter even $ filter isLessThan4Mil $ calcFibs 50
+solve = sum $ fibsLessThan4MilAndEven 
