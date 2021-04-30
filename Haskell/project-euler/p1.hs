cd50c9756998fea885f7fa41e494ae2dc2b64e2c
Clark Kozak <clarkkozak@protonmail.com>
2021-04-29 21:25:17 -0600

    haskell : p1 optimize

--- Haskell/project-euler/p1.hs
+++ Haskell/project-euler/p1.hs
@@ -1,9 +1,18 @@
 nums :: [Int]
 nums = [1..999] -- we want numbers below (and not equal to) 1000
 
-solve :: [Int] -> Int
-solve numbers = foldr (+) 0 $ filter (\x -> x `mod` 3 == 0 || x `mod` 5 == 0) numbers
+naiveSolve :: [Int] -> Int
+naiveSolve numbers = sum $ filter (\x -> x `mod` 3 == 0 || x `mod` 5 == 0) numbers
+
+
+sumSeries :: Int -> Int
+sumSeries n = (n * (n + 1)) `div` 2
+
+sumSeriesInterval :: Int -> Int -> Int
+sumSeriesInterval x y = x * (sumSeries (y `div` x))
+
+solve = sumSeriesInterval 3 1000 + sumSeriesInterval 5 1000 - sumSeriesInterval 15 1000
 
 ans :: Int
-ans = solve nums
+ans = solve 
 
