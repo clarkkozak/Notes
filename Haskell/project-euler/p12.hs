8e06fdd2bdf0ad75e4caa1722e017f514ead12ec
Clark Kozak <clarkkozak@protonmail.com>
2021-05-03 16:23:56 -0600

    haskell: p12 (i think i'm going to be sick)
    https://stackoverflow.com/questions/4771083/help-me-find-the-problem-with-my-solution-to-project-euler-12-in-haskell
    
    I mean holy shit. Project Euler really does force you do learn math
    
    Or forces you do uses 3rd party packages if you are a cheater like me
    
    I think I need to take a break from this stuff.
    
    This has to be the best solution i came across:
    https://zach.se/project-euler-solutions/12/

--- Haskell/project-euler/p12.hs
+++ Haskell/project-euler/p12.hs
@@ -1,19 +1,16 @@
+import Data.Numbers.Primes (primeFactors)
+import Control.Monad (filterM)
+import Data.List
+import Data.Maybe
+
 triangleNumber :: Integer -> Integer
 triangleNumber n = n * (n+1) `div` 2
 
 triangleNumbers :: [Integer]
 triangleNumbers = map triangleNumber [1..]
 
-factorList :: Integer -> [Integer]
-factorList value = factorsGreaterOrEqual 1
-  where
-    factorsGreaterOrEqual test
-      | (test == value) = [value]
-      | (value `mod` test == 0) = test : restOfFactors
-      | otherwise = restOfFactors
-      where restOfFactors = factorsGreaterOrEqual (test + 1)
+powerSet = filterM (const [True, False])
 
-factorListOver5divsors :: Integer -> Bool
-factorListOver5divsors list = (length $ factorList list) < 500
+factors = map product . nub . powerSet . primeFactors
 
--- optimization needed: map length $ map factorList triangleNumbers
+solve = head $ fromJust $ find (\x -> length x > 500) $ map factors triangleNumbers
