efbf139adfc68ff54e93974ed75fc342cd0815fe
Clark Kozak <clarkkozak@protonmail.com>
2021-07-03 23:55:09 -0600

    rust: 1. Getting Started
    `rustup docs --book`

---
+++ Rust/hello.rs
@@ -0,0 +1,11 @@
+// This is the main function
+
+fn main() {
+    // Print text to the console
+    println!("Hello world");
+    println!("I am learning Rust");
+
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+    println!("It's 10 yo");
+}
