9247f0ffb31177b547835ae51e4907829fc74b72
Clark Kozak <clarkkozak@protonmail.com>
2022-03-06 09:42:04 -0700

    rust: discovery with micro:bit
    I made some changes prior to commit. Should have committed first
    then I could have made changes to 05-led-roulette

---
+++ Rust/discovery/microbit/src/05-led-roulette/src/main.rs
@@ -0,0 +1,19 @@
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use microbit::board::Board;
+use microbit::hal::prelude::*;
+use panic_halt as _;
+
+#[entry]
+fn main() -> ! {
+    let mut board = Board::take().unwrap();
+
+    board.display_pins.col1.set_low().unwrap();
+    board.display_pins.row1.set_high().unwrap();
+
+    // infinite loop; just so we don't leave this stack frame
+    loop {}
+}
