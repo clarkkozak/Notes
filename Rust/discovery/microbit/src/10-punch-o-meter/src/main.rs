dca7f02466f10ca1d5435cd9303cc4c41e9786de
Clark Kozak <clarkkozak@protonmail.com>
2022-04-03 11:43:31 -0700

    rust(microbit): collect accelerometer data

--- Rust/discovery/microbit/src/10-punch-o-meter/src/main.rs
+++ Rust/discovery/microbit/src/10-punch-o-meter/src/main.rs
@@ -3,13 +3,27 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use rtt_target::rtt_init_print;
 use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+#[cfg(feature = "v2")]
+use microbit::{hal::twim, pac::twim0::frequency::FREQUENCY_A};
+
+use lsm303agr::{AccelOutputDataRate, Lsm303agr};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
-    loop {}
+    let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+
+    loop {
+        let data = sensor.accel_data().unwrap();
+        rprintln!("x {}, y {}, z {}", data.x, data.y, data.z)
+    }
 }
