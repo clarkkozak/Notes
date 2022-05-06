f1203622b4ba511031a0a0868613939ba670fa46
Clark Kozak <clarkkozak@protonmail.com>
2022-05-06 10:25:14 -0700

    rust: hyper POST data
    lol I used to think something like this is "low level"

--- Rust/hyper-playground/echo/src/main.rs
+++ Rust/hyper-playground/echo/src/main.rs
@@ -1,10 +1,39 @@
+use futures::TryStreamExt;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
-use std::convert::Infallible;
-use std::net::SocketAddr;
 
-async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello World!")))
+async fn hello(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from("Try POSTing data to /echo");
+        }
+        (&Method::POST, "/echo") => {
+            *response.body_mut() = req.into_body();
+        }
+        (&Method::POST, "/echo/uppercase") => {
+            let mapping = req.into_body().map_ok(|chunk| {
+                chunk
+                    .iter()
+                    .map(|byte| byte.to_ascii_uppercase())
+                    .collect::<Vec<u8>>()
+            });
+
+            *response.body_mut() = Body::wrap_stream(mapping);
+        }
+        (&Method::POST, "/echo/reversed") => {
+            let full_body = hyper::body::to_bytes(req.into_body()).await?;
+
+            let reversed = full_body.iter().rev().cloned().collect::<Vec<u8>>();
+
+            *response.body_mut() = reversed.into();
+        }
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    };
+    Ok(response)
 }
 
 #[tokio::main]
@@ -15,7 +44,7 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         // This is the `Service` that will handle the connection.
         // `service_fn` is a helper to convert a function that
         // returns a Response into a `Service`.
-        async { Ok::<_, Infallible>(service_fn(hello)) }
+        async { Ok::<_, hyper::Error>(service_fn(hello)) }
     });
 
     let addr = ([127, 0, 0, 1], 3000).into();
