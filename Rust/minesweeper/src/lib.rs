e1012fbd47d6fc0c43de73eea4d04fc38854d53c
Clark Kozak <clarkkozak@protonmail.com>
2022-05-19 08:58:29 -0700

    rust(ms): wasm-bindgen
    - index.html to serve website
    - use  ES module to import built wasm
    -  conditional compilation for wasm and normal builds
      - `rand` doesn't support wasm

--- Rust/minesweeper/src/lib.rs
+++ Rust/minesweeper/src/lib.rs
@@ -2,3 +2,14 @@ mod minesweeper;
 mod random;
 
 use minesweeper::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str); // From browser
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name)); // Use it in Rust code..?
+}
--- Rust/minesweeper/src/random.rs
+++ Rust/minesweeper/src/random.rs
@@ -1,7 +1,21 @@
+#[cfg(not(target_family = "wasm"))]
 use rand::{thread_rng, Rng};
+use wasm_bindgen::prelude::*;
 
+#[cfg(not(target_family = "wasm"))]
 pub fn random_range(min: usize, max: usize) -> usize {
     let mut rng = thread_rng();
 
     rng.gen_range(min..max)
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = Math)] // I don't understand how this works
+    fn random() -> f64;
+}
+
+#[cfg(target_family = "wasm")]
+pub fn random_range(min: usize, max: usize) -> usize {
+    (random() * (max - min) as f64).floor() as usize + min // now  we have Math.random?
 }
