4685b6a90c1d4aa83df4bc1b65f218f846f26cf0
Clark Kozak <clarkkozak@protonmail.com>
2022-05-19 09:53:52 -0700

    rust(ms): display as grid and interact
    BUG: the field says 0 when  it has a bomb next to it

--- Rust/minesweeper/src/lib.rs
+++ Rust/minesweeper/src/lib.rs
@@ -1,6 +1,8 @@
 mod minesweeper;
 mod random;
 
+use std::cell::RefCell;
+
 use minesweeper::*;
 use wasm_bindgen::prelude::*;
 
@@ -9,12 +11,23 @@ extern "C" {
     fn alert(s: &str); // From browser
 }
 
+// WASM is only 1 thread anyone, so this is  okay?
+thread_local! {
+    // RefCell has "interior mutability"
+    // Circumvents the  borrow check and add it to the runtime checker?
+    // Adds a bit of  overhead
+    static MINESWEEPER: RefCell<Minesweeper> = RefCell::new(Minesweeper::new(10, 10, 15));
+}
+
 #[wasm_bindgen(js_name = getState )]
 pub fn get_state() -> String {
-    let ms = Minesweeper::new(10, 10, 5);
+    MINESWEEPER.with(|ms| ms.borrow().to_string())
+}
 
-    // to_string works due to the Display trait
-    // Display automatically implements to_string()
-
-    ms.to_string()
+// Only strings and numbers for WASM; no tuples
+#[wasm_bindgen(js_name = openField)]
+pub fn open_field(x: usize, y: usize) {
+    MINESWEEPER.with(|ms| {
+        ms.borrow_mut().open((x, y));
+    })
 }
