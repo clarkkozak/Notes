52525f2fe4f02a4d6a87a761e5084d7c1f4d11c8
Clark Kozak <clarkkozak@protonmail.com>
2022-05-19 08:08:45 -0700

    rust(mb): puncho o meter (out dated)
    Idk when I did this

--- Rust/discovery/microbit/src/10-punch-o-meter/src/main.rs
+++ Rust/discovery/microbit/src/10-punch-o-meter/src/main.rs
@@ -12,26 +12,63 @@ use microbit::{hal::prelude::*, hal::timer::Timer, hal::twim, pac::twim0::freque
 
 use lsm303agr::{AccelOutputDataRate, AccelScale, Lsm303agr};
 
+use nb::Error;
+
 #[entry]
 fn main() -> ! {
+    const THRESHOLD: f32 = 0.5;
+
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
     let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
 
-    let mut timer = Timer::new(board.TIMER0);
+    let mut countdown = Timer::new(board.TIMER0);
+    let mut timer = Timer::new(board.TIMER1);
 
     let mut sensor = Lsm303agr::new_with_i2c(i2c);
     sensor.init().unwrap();
     sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
     sensor.set_accel_scale(AccelScale::G16).unwrap();
 
-    let mut buffer: Vec<i32, 32> = Vec::new();
+    let mut buffer: Vec<i32, 64> = Vec::new();
+    let mut max_g = 0.;
+    let mut measuring = false;
 
     loop {
-        let data = sensor.accel_data().unwrap();
-        timer.delay_ms(500_u32);
-        buffer.push(data.x).unwrap();
-        rprintln!("buffer {:?}", buffer);
+        // Wait till the status is complete
+        while !sensor.accel_status().unwrap().xyz_new_data {}
+
+        let g_x = sensor.accel_data().unwrap().x as f32 / 1000.0;
+
+        if measuring {
+            match countdown.wait() {
+                Err(Error::WouldBlock) => {
+                    if g_x > max_g {
+                        max_g = g_x;
+                    }
+                }
+                // Countdown is done
+                Ok(_) => {
+                    rprintln!("Max acceleration: {}g", max_g);
+
+                    // Rest
+                    max_g = 0.;
+                    measuring = false;
+                }
+                Err(Error::Other(_)) => {
+                    unreachable!();
+                }
+            }
+        } else if g_x > THRESHOLD {
+            rprintln!("Start!");
+            measuring = true;
+            max_g = g_x;
+
+            // The documentation notes that the timer works at a frequency
+            // of 1 Mhz, so in order to wait for 1 second we have to
+            // set it to 1_000_000 ticks.
+            countdown.start(1_000_000_u32);
+        }
     }
 }
