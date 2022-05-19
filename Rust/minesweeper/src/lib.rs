17033e6f57a710949b8ef53cc54f7212348299ad
Clark Kozak <clarkkozak@protonmail.com>
2022-05-19 09:02:03 -0700

    rust(ms): show game state in the console

--- Rust/minesweeper/src/lib.rs
+++ Rust/minesweeper/src/lib.rs
@@ -9,7 +9,12 @@ extern "C" {
     fn alert(s: &str); // From browser
 }
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name)); // Use it in Rust code..?
+#[wasm_bindgen(js_name = getState )]
+pub fn get_state() -> String {
+    let ms = Minesweeper::new(10, 10, 5);
+
+    // to_string works due to the Display trait
+    // Display automatically implements to_string()
+
+    ms.to_string()
 }
